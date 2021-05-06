b8c16ec42bf768e55a9e7db9bef8724671e26925
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-05-06 13:45:50 +0100

    worked on the setup

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+
+const buttons = document.querySelectorAll('.question-btn');
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        console.log(e);
+    });
+});
