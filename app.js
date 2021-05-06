2237795a57843c8afb9c0f02b9f0585e26325d86
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-05-06 14:50:45 +0100

    completed project

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-//using selectors inside the element
+
 // traversing the dom
 
-const buttons = document.querySelectorAll('.question-btn');
+// const buttons = document.querySelectorAll('.question-btn');
 
-buttons.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        console.log(e);
+// buttons.forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
+
+
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach( (questionContainer) => {
+    // console.log(questionContainer);
+    const clickBtn = questionContainer.querySelector('.question-btn');
+    clickBtn.addEventListener('click', () => {
+        questions.forEach( (item) => {
+            if(item !== questionContainer){
+                item.classList.remove('show-text')
+            }
+        });
+        questionContainer.classList.toggle('show-text');
     });
 });
